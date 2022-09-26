a3db6b69e1a574ac2589da0c9d927353f3670530
Ridesh Raj <rideshr101@gmail.com>
2022-09-26 21:10:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header("Spideyyy..!!! Mannnn");
 streamlit.text("Spideyyy..!!! Mannnn");
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
-# my_fruit_list = my_fruit_list.set_index("Fruit");
+my_fruit_list = my_fruit_list.set_index("Fruit");
 
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index));
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ["Apple", "Strawberries"]);
 streamlit.dataframe(my_fruit_list);
