a0901dc86fdea43ea36d5f98cc1c7f0741ae7b97
Ridesh Raj <rideshr101@gmail.com>
2022-09-26 20:51:01 +0530

    Update streamlit_app.py
    added csv file...

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
-import streamlit
+import streamlit;
+import pandas as pd;
 
-streamlit.title("Spideyyy..!!! Mannnn")
-streamlit.header("Spideyyy..!!! Mannnn")
-streamlit.text("Spideyyy..!!! Mannnn")
+streamlit.title("Spideyyy..!!! Mannnn");
+streamlit.header("Spideyyy..!!! Mannnn");
+streamlit.text("Spideyyy..!!! Mannnn");
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+streamlit.dataframe(my_fruit_list);
