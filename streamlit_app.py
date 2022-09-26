20037cf3d02b05c98a31004aa3e0c036334e8971
Ridesh Raj <rideshr101@gmail.com>
2022-09-26 21:01:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.header("Spideyyy..!!! Mannnn");
 streamlit.text("Spideyyy..!!! Mannnn");
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index));
 streamlit.dataframe(my_fruit_list);
